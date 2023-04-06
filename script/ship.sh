24f609d5281ad6435f08f5746fb60f06434d303a
Jeremy Banka <hello@jeremybanka.com>
2023-04-06 09:41:03 -0700

    🔨 separate scripts

--- script/ship.sh
+++ script/ship.sh
@@ -6,10 +6,12 @@ script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 cd "$script_dir"
 
+./pack.sh
+
 cd ..
 
-npm version patch
+npm publish
 
 cd react
 
-npm version patch
+npm publish
